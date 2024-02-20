a73aebc19801089c2660eb14d90a192b155439f9
diogo-has <dioglois.games@gmail.com>
2024-02-20 12:29:00 -0300

    picadms

--- teste/script.js
+++ teste/script.js
@@ -1,5 +1,25 @@
-fetch('json.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+var questaoAtual = 0;
 
-  
+fetch("json.json")
+  .then(result => result.json())
+  .then(result => result.content.questions[questaoAtual])
+  .then(questao => {
+    document.getElementById("num").textContent = questao.id;
+    document.getElementById("title").innerHTML = questao.body;
+    
+    var altern = document.getElementById("altern");
+    questao.options.forEach(option => {
+      var opt = document.createElement('input');
+      opt.type = "radio";
+      opt.name = questao.id;
+      opt.id = option.id;
+
+      var optlbl = document.createElement('label');
+      optlbl.for = option.id;
+      optlbl.textContent = option.content;
+
+
+      altern.appendChild(opt);
+      altern.appendChild(optlbl);
+    });
+  })
