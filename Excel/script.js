b09c8bee88e96c25e4c8218707cc088f36143b0b
diogo.haschich <dioglois.games@gmail.com>
2024-03-12 09:36:56 -0300

    equicel

--- Excel/script.js
+++ Excel/script.js
@@ -9,4 +9,25 @@ function adicionarItem() {
     }
 
     var tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
+    var novaLinha = tabela.insertRow(tabela.rows.length);
+
+    var celulasNome = novaLinha.insertCell(0);
+    var celulasValor = novaLinha.insertCell(1);
+    var celulasQuantidade = novaLinha.insertCell(2);
+
+    celulasNome.innerHTML = nome;
+    celulasValor.innerHTML = valor;
+    celulasQuantidade.innerHTML = quantidade;
+
+    document.getElementById('nome').value = '';
+    document.getElementById('valor').value = '';
+    document.getElementById('quantidade').value = '';
+}
+
+function exportarParaExcel() {
+    var tabela = document.getElementById('tabela');
+    var nomeArquivo = 'tabela.xlsx';
+    var wb = XLSX.utils.table_to_book(tabela, {sheet: 'Tabela'});
+    XLSX.writeFile(wb, nomeArquivo);
+
 }
